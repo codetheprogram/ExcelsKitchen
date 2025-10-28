7e2bf6dba2497def6c85c722fcb0bf5ab7c1d57d
peter excellence <bobosmay5@gmail.com>
2025-10-28 11:08:17 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <div id="App">
       <Platform />
       </div>
-      
+       
     </>
   )
 }
--- src/components/Platform.jsx
+++ src/components/Platform.jsx
@@ -8,20 +8,14 @@ import Food4 from '../assets/Food4.png'
 const Platform = () => {
   return (
     <>
-      <div id='Wheel'>
-        <img className='food1' src={Food1} />
-        <img className='food2' src={Food2} />
-        <img className='food3' src={Food3} />
-        <img className='food4' src={Food4} />
-      </div>
-
+    
       <div id='Wheel-description'>
         <div id='des-containerA'>
         <p id='first-description'>#1 Most loved dish</p>
         <p id='second-descriptionA'>LETTUCE</p>
         <p id='second-descriptionB'>PAK BEEF</p>
         </div>
-        <div id='des-containerB'>
+        {/* <div id='des-containerB'>
         <p id='first-description'>#2 Most loved dish</p>
         <p id='second-descriptionA'>SUSHI</p>
         <p id='second-descriptionB'>DE SALA</p>
@@ -35,8 +29,15 @@ const Platform = () => {
         <p id='first-description'>#4 Most loved dish</p>
         <p id='second-descriptionA'>BETA</p>
         <p id='second-descriptionB'>JELLOF</p>
-        </div>
+        </div> */}
       </div>
+        <div id='Wheel'>
+        <img className='food1' src={Food1} />
+        <img className='food2' src={Food2} />
+        <img className='food3' src={Food3} />
+        <img className='food4' src={Food4} />
+      </div>
+
     </>
   )
 }
